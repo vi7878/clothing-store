65a9a0bacd77c8e0cd1ae316b882c552844c3b80
Vitalii <151910698+vi7878@users.noreply.github.com>
2026-06-05 18:22:29 +0300

    Revert "feat: implement authentication, account management and order flow"

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,11 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import ShopContextProvider from './context/ShopContext.jsx'
-import { AuthProvider } from './context/AuthContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <AuthProvider> {/* <--- Обгорни ShopContextProvider та App */}
-        <ShopContextProvider>
-          <App />
-        </ShopContextProvider>
-      </AuthProvider>
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
   </BrowserRouter>,
 )
